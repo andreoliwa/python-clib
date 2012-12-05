4ba446459a9bab426404346fdad688d8af33a0f3
Wagner Augusto Andreoli <wagnerandreoli@gmail.com>
2012-12-05 21:54:38 -0200

    git-id-to-svn-link.sh: Change a git-svn-id to a SVN link in data received from stdin. publish.sh: Copies extra files that could not be copied over Samba.

--- publish.sh
+++ publish.sh
@@ -23,20 +23,26 @@ V_PROJECTS="$*"
 echo
 echo "Publicando os seguintes projetos: $V_PROJECTS"
 
+# Copies extra files that could not be copied over Samba
+# rsync: mkstemp "/home/wagner/src/remote/common/classes/._spiffy_version_control.class.php.pc0QK1" failed: No such file or directory (2)
+# rsync: mkstemp "/home/wagner/src/remote/common/classes/._spiffy_version_control_interface.class.php.P4rcfQ" failed: No such file or directory (2)
+# rsync: mkstemp "/home/wagner/src/remote/common/classes/._spiffy_version_control_svn.class.php.TekeKE" failed: No such file or directory (2)
+# rsync error: some files/attrs were not transferred (see previous errors) (code 23) at main.c(1070) [sender=3.0.9]
+cp -vu $G_WORK_SRC_DIR/dev_htdocs/common/classes/_spiffy_* ~/src/remote/common/classes/
+
 for PASS in {1..2} ; do
 	echo
 	echo "rsync $G_WORK_SRC_DIR -> $FOLHA_SRC_PUBLISH_DIR (passo $PASS)"
 
+	#V_COMMON_CMD="rsync -rlD --delete --progress --exclude-from=$G_WORK_SRC_DIR/dev_bin/.gitignore --exclude=_archive* --modify-window=2"
 	V_COMMON_CMD="rsync -huzr --delete --progress --exclude-from=$G_WORK_SRC_DIR/dev_bin/.gitignore --exclude=_archive* --modify-window=2 --times --omit-dir-times"
 
 	V_ALL_DIRS="$V_COMMON_DIRS $(project-folders.sh $V_PROJECTS)"
 	for V_DIR in $V_ALL_DIRS ; do
 		V_LOCAL_DIR="$G_WORK_SRC_DIR/$V_DIR/"
-		#echo $V_LOCAL_DIR
 		V_REMOTE_DIR=$(echo "$FOLHA_SRC_PUBLISH_DIR/$V_DIR/" | sed 's#dev_htdocs/##')
 		if [ -d "$V_LOCAL_DIR" ] ; then
 			mkdir -p "$V_REMOTE_DIR"
-			#echo "$V_COMMON_CMD $V_LOCAL_DIR $V_REMOTE_DIR"
 			V_SYNCED_FILES=$($V_COMMON_CMD "$V_LOCAL_DIR" "$V_REMOTE_DIR" | grep -v -e bytes/sec -e 'total size' -e 'sending incremental' -e '^$' -e 'xfer#')
 			if [ -n "$V_SYNCED_FILES" ] ; then
 				echo ">>> Publicando diretorio $V_DIR"
