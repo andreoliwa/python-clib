5fb3b4141d1234c617044aa14e7a54dc50c68898
Wagner Augusto Andreoli <wagnerandreoli@gmail.com>
2013-03-17 13:19:35 -0300

    sh-new-script.sh: Allow multiple scripts in the command line.

--- sh-new-script.sh
+++ sh-new-script.sh
@@ -4,31 +4,31 @@ usage() {
 Create a new shell script (if it does not exist), or else opens the existing script.
 
 OPTIONS
--f  Desired file name (.sh extension is used if none is given)
+-f  Desired file name (default .sh extension); you can use this option multiple times
 -n  Dry-run: don't create the script, only show its full name
 -h  Help"
 	exit $1
 }
 
-V_SCRIPT_NAME=
+V_ALL_SCRIPTS=
 V_DRY_RUN=
 while getopts "f:nh" V_ARG ; do
 	case $V_ARG in
-	f)	V_SCRIPT_NAME=$OPTARG ;;
+	f)	V_ALL_SCRIPTS="$V_ALL_SCRIPTS $OPTARG" ;;
 	n)	V_DRY_RUN="(DRY-RUN) " ;;
 	h)	usage 1 ;;
 	?)	usage 2 ;;
 	esac
 done
 
-if [ -z "$V_SCRIPT_NAME" ] ; then
+if [ -z "$V_ALL_SCRIPTS" ] ; then
 	# If it's not an option, we'll consider it text
 	if [ "${1:0:1}" != '-' ] ; then
-		V_SCRIPT_NAME="$1"
+		V_ALL_SCRIPTS="$1"
 	fi
 fi
 
-if [ -z "$V_SCRIPT_NAME" ] ; then
+if [ -z "$V_ALL_SCRIPTS" ] ; then
 	echo -e "Please suplly a script name with the -f option, or as the first argument in the command line.\n"
 	usage 3
 fi
@@ -40,44 +40,7 @@ open_script_file() {
 	fi
 }
 
-if [ -f "$V_SCRIPT_NAME" ] ; then
-	echo "${V_DRY_RUN}Opening existing shell script $V_SCRIPT_NAME (full path: $(readlink -e $V_SCRIPT_NAME))"
-	open_script_file $V_SCRIPT_NAME
-	exit 0
-fi
-
-# Uses the .sh extension if the file doesn't have one
-V_EXTENSION="${V_SCRIPT_NAME##*.}"
-if [ "$V_EXTENSION" == "$V_SCRIPT_NAME" ] ; then
-	V_SCRIPT_NAME=$V_SCRIPT_NAME.sh
-fi
-
-V_FULL_PATH=
-V_SCRIPT_DIR="$(dirname $V_SCRIPT_NAME)"
-if [ "$V_SCRIPT_DIR" = '.' ] ; then
-	# If no directory was informed, then searches the path
-	V_EXISTS="$(path-find.sh $V_SCRIPT_NAME)"
-	if [ -f "$V_EXISTS" ] ; then
-		V_FULL_PATH="$V_EXISTS"
-	else
-		V_FULL_PATH="$G_NEW_SCRIPTS_DIR/$V_SCRIPT_NAME"
-	fi
-else
-	V_FULL_PATH=$V_SCRIPT_NAME
-fi
-
-if [ -f "$V_FULL_PATH" ] ; then
-	echo "${V_DRY_RUN}Opening existing shell script $V_FULL_PATH"
-	open_script_file $V_FULL_PATH
-	exit 0
-fi
-
-echo "${V_DRY_RUN}Creating shell script $V_FULL_PATH"
-if [ -n "$V_DRY_RUN" ] ; then
-	exit 0
-fi
-
-echo '#!/bin/bash
+V_DEFAULT_TEXT='#!/bin/bash
 usage() {
 	echo "Usage: $(basename $0) [options]
 Type here your brief description of the shell script.
@@ -92,6 +55,43 @@ while getopts "h" V_ARG ; do
 	h)	usage 1 ;;
 	?)	usage 2 ;;
 	esac
-done' > $V_FULL_PATH
-chmod +x $V_FULL_PATH
-open_script_file $V_FULL_PATH
+done'
+
+for V_SCRIPT_NAME in $V_ALL_SCRIPTS ; do
+	if [ -f "$V_SCRIPT_NAME" ] ; then
+		echo "${V_DRY_RUN}Opening existing shell script $V_SCRIPT_NAME (full path: $(readlink -e $V_SCRIPT_NAME))"
+		open_script_file $V_SCRIPT_NAME
+	else
+		# Uses the .sh extension if the file doesn't have one
+		V_EXTENSION="${V_SCRIPT_NAME##*.}"
+		if [ "$V_EXTENSION" == "$V_SCRIPT_NAME" ] ; then
+			V_SCRIPT_NAME=$V_SCRIPT_NAME.sh
+		fi
+
+		V_FULL_PATH=
+		V_SCRIPT_DIR="$(dirname $V_SCRIPT_NAME)"
+		if [ "$V_SCRIPT_DIR" = '.' ] ; then
+			# If no directory was informed, then searches the path
+			V_EXISTS="$(path-find.sh $V_SCRIPT_NAME)"
+			if [ -f "$V_EXISTS" ] ; then
+				V_FULL_PATH="$V_EXISTS"
+			else
+				V_FULL_PATH="$G_NEW_SCRIPTS_DIR/$V_SCRIPT_NAME"
+			fi
+		else
+			V_FULL_PATH=$V_SCRIPT_NAME
+		fi
+
+		if [ -f "$V_FULL_PATH" ] ; then
+			echo "${V_DRY_RUN}Opening existing shell script $V_FULL_PATH"
+			open_script_file $V_FULL_PATH
+		else
+			echo "${V_DRY_RUN}Creating shell script $V_FULL_PATH"
+			if [ -z "$V_DRY_RUN" ] ; then
+	 			echo $V_DEFAULT_TEXT > $V_FULL_PATH
+				chmod +x $V_FULL_PATH
+				open_script_file $V_FULL_PATH
+			fi
+		fi
+	fi
+done
