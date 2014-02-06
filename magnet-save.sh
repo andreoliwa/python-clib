789920bc19b4e691508e6beb2127bd2be75a6e77
Wagner AA <wagnerandreoli@gmail.com>
2014-02-05 22:03:35 -0200

    Some pages with magnet links were not being properly recognized.

--- magnet-save.sh
+++ magnet-save.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 V_URL="$1"
-V_MAGNET_LINK=$(lynx --dump $V_URL | grep 'magnet:' | sed 's/.\+magnet/magnet/' | sort | uniq)
+V_MAGNET_LINK=$(wget -O - $V_URL | grep 'magnet:' | sed 's/.\+magnet/magnet/' | sort | uniq)
 
 if [ -z "$V_MAGNET_LINK" ] ; then
 	echo "This URL has no magnet links: $V_URL"
