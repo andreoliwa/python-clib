d5820fdf06b6f46e5294faf7a2195cc353c05e90
Wagner Augusto Andreoli <wagnerandreoli@gmail.com>
2015-02-12 23:19:47 +0100

    Add another alias for git stash

--- dotfiles/andreoli.zsh
+++ dotfiles/andreoli.zsh
@@ -4,6 +4,7 @@ alias gld='git log ...develop'
 alias grepalias='alias | grep '
 alias gdm='git diff master'
 alias gdd='git diff develop'
+alias gstl='git stash list'
 
 if [[ "${OSTYPE//[0-9.]/}" == 'darwin' ]]; then
     cd ~/eatfirst/cave
