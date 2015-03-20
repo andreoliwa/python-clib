055fea526cad8816c19541432827f98f03863a2d
Wagner Augusto Andreoli <wagnerandreoli@gmail.com>
2015-03-21 00:42:18 +0100

    Add more cleaning examples to the Docker script

--- legacy/docker-vacuum.sh
+++ legacy/docker-vacuum.sh
@@ -10,3 +10,13 @@ echo 'docker images -q -a | xargs docker rmi -f'
 echo
 echo '# Remove desired images'
 echo "docker images -a | grep -e '<none>' -e cave_ | awk '{print \$3}' | xargs docker rmi -f"
+
+# http://stackoverflow.com/questions/17236796/how-to-remove-old-docker-containers
+# Old containers
+# docker ps -a | grep 'weeks ago' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+
+# Stopped
+# docker ps -a | grep Exited | awk '{print $1}' | xargs --no-run-if-empty docker rm
+
+# By status
+# docker ps -aq -f status=exited
