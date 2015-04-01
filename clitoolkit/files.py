4928ab311bb4378b83adbb776dace9b7741736e6
Wagner Augusto Andreoli <wagnerandreoli@gmail.com>
2015-04-01 02:45:57 +0200

    #8 Create links for directories listed in config.ini

--- clitoolkit/files.py
+++ clitoolkit/files.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """Files, symbolic links, operating system utilities."""
 import os
-from clitoolkit import LOGGER, read_config, save_config
+
+from clitoolkit import LOGGER, read_config, save_config, CONFIG
+
+
+SECTION_SYMLINKS_FILES = 'symlinks/files'
+SECTION_SYMLINKS_DIRS = 'symlinks/dirs'
 
 
 def create_symbolic_links():
@@ -13,29 +18,36 @@ def create_symbolic_links():
         return
     LOGGER.info("Directory with dot files: '%s'", dot_files_dir)
 
+    LOGGER.info("Creating links for files in [%s]", SECTION_SYMLINKS_FILES)
     links = {}
     cut = len(dot_files_dir) + 1
     for root, _, files in os.walk(dot_files_dir):
         for one_file in files:
             source_file = os.path.join(root, one_file)
             key = source_file[cut:]
-            raw_link_name = read_config('symlinks/files', key, '')
+            raw_link_name = read_config(SECTION_SYMLINKS_FILES, key, '')
             links[key] = (source_file, raw_link_name)
     # http://stackoverflow.com/questions/9001509/how-can-i-sort-a-python-dictionary-sort-by-key/13990710#13990710
     for key in sorted(links):
         (source_file, raw_link_name) = links[key]
-        create_link(key, source_file, raw_link_name)
+        create_link(key, source_file, raw_link_name, False)
 
-    # TODO Create links for directories
+    LOGGER.info("Creating links for dirs in [%s]", SECTION_SYMLINKS_DIRS)
+    if CONFIG.has_section(SECTION_SYMLINKS_DIRS):
+        for key in CONFIG.options(SECTION_SYMLINKS_DIRS):
+            raw_link_name = read_config(SECTION_SYMLINKS_DIRS, key, '')
+            create_link(key, key, raw_link_name, True)
+
     save_config()
 
 
-def create_link(key, source_file, raw_link):
+def create_link(key, source_file, raw_link, is_dir):
     """Check and create a symbolic link.
 
     :param key: Key name in the config.ini file.
     :param source_file: Full path to the source file that will be linked.
     :param raw_link: Raw destination link taken from config.ini.
+    :param is_dir: Consider the paths as directories.
     :return:
     """
     def message(text, logger_func=LOGGER.warning):
@@ -47,36 +59,38 @@ def create_link(key, source_file, raw_link):
         """
         logger_func("%s -> '%s': %s", key, final_link, text)
 
-    create = True
+    if is_dir:
+        # In config.ini, a dir can contain the char "~" in the key
+        source_file = os.path.expanduser(source_file)
+
     final_link = raw_link
     if not raw_link:
         message('empty link in the config file')
-        create = False
+        return
+
+    expanded_link = os.path.expanduser(raw_link)
+    if raw_link == '~' or expanded_link.endswith('/'):
+        # Append the basename when the dir ends with a slash
+        final_link = os.path.join(expanded_link, os.path.basename(source_file))
     else:
-        expanded_link = os.path.expanduser(raw_link)
-        if os.path.isdir(expanded_link):
-            final_link = os.path.join(expanded_link, os.path.basename(source_file))
-        else:
-            final_link = expanded_link
+        final_link = expanded_link
 
-        if os.path.islink(final_link):
-            create = False
-            try:
-                if os.path.samefile(source_file, final_link):
-                    message('link already exists', LOGGER.info)
-                else:
-                    message('link already exists, but points to a different file')
-            except FileNotFoundError as err:
-                message('file not found? {}'.format(err), LOGGER.error)
-        elif os.path.isfile(final_link):
-            create = False
+    if os.path.islink(final_link):
+        try:
             if os.path.samefile(source_file, final_link):
-                message("an identical file already exists; it can be safely replaced")
-                # TODO If files are identical, rename the target and continue with link creation
-            else:
-                message("a file with the same name already exists, and they are not the same. "
-                        "Try comparing the files with:\nmeld '{}' '{}'".format(source_file, final_link), LOGGER.error)
+                message('link already exists', LOGGER.info)
+                return
+            message('link already exists, but points to a different file')
+        except FileNotFoundError as err:
+            message('file not found? {}'.format(err), LOGGER.error)
+        return
+    elif os.path.isfile(final_link):
+        if os.path.samefile(source_file, final_link):
+            message("an identical file already exists; it can be manually replaced")
+            return
+        message("a file with the same name already exists, and they are not the same. "
+                "Try comparing the files with:\nmeld '{}' '{}'".format(source_file, final_link), LOGGER.error)
+        return
 
-    if create:
-        os.symlink(source_file, final_link, target_is_directory=False)
-        message('link created', LOGGER.info)
+    os.symlink(source_file, final_link, target_is_directory=not is_dir)
+    message('link created', LOGGER.info)
