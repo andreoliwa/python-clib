1d3f53d108941f984804d9494b1eb9a02d45f793
Augusto W. Andreoli <andreoliwa@gmail.com>
2020-05-17 20:32:21 +0200

    feat: add roundrobin function

---
+++ src/clib/iter.py
@@ -0,0 +1,23 @@
+"""Tools for iterators and iterables."""
+from itertools import cycle, islice
+
+
+def roundrobin(*iterables):
+    """Get one item from each iterable.
+
+    Recipe credited to George Sakkis.
+    Taken from https://docs.python.org/3/library/itertools.html#itertools-recipes
+
+    >>> list(roundrobin('ABC', 'D', 'EF'))
+    ['A', 'D', 'E', 'B', 'F', 'C']
+    """
+    num_active = len(iterables)
+    nexts = cycle(iter(it).__next__ for it in iterables)
+    while num_active:
+        try:
+            for next in nexts:
+                yield next()
+        except StopIteration:
+            # Remove the iterator we just exhausted from the cycle.
+            num_active -= 1
+            nexts = cycle(islice(nexts, num_active))
